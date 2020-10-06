27b6801e6ad4c185299e4a6ed8737f74dfe197fa
EbonyChantelle <ebonychantelle@icloud.com>
2020-10-06 21:30:40 +0100

    Homework/Session 5 Updates

--- Portfolio/React/mybookcase/src/components/Book.js
+++ Portfolio/React/mybookcase/src/components/Book.js
@@ -1,58 +1,89 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Book = (props) => {
   let {
-    volumeInfo: { title, authors, description, imageLinks },
-    saleInfo,
+    volumeInfo: {
+      title,
+      authors,
+      description,
+      imageLinks: { thumbnail, smallThumbnail },
+    },
+    saleInfo: { listPrice },
   } = props.book;
 
+  function addBook(title) {
+    console.log(`The Book ${title} Was Clicked`);
+  }
+
   const formatter = new Intl.NumberFormat("en-GB", {
-     style: "currency",
-     currency: "GBP",
+    style: "currency",
+    currency: "GBP",
   });
 
-  let amount = () => {
-    if (saleInfo.retailPrice) {
-      let amount = saleInfo.retailPrice.amount;
-      return formatter.format(amount);
-    } else {
-      let amount = "No Amount Set";
-      return amount;
-    }
-  };
+  // let amount = () => {
+  //   if (saleInfo.retailPrice) {
+  //     let amount = saleInfo.retailPrice.amount;
+  //     return formatter.format(amount);
+  //   } else {
+  //     let amount = "No Amount Set";
+  //     return amount;
+  //   }
+  // };
 
-  let url = () => {
-    if (imageLinks) { // Test if the imagesLinks object exists
-      if (imageLinks.thumbnail) { // Test if the thumbnail property exists
-        let url = imageLinks.thumbnail;
-        return url;
-      }
-      else if(imageLinks.smallThumbnail) { // Test if the smallThumbnail property exists
-        let url = imageLinks.smallThumbnail;
-        return url;
-      }
-      else { // imageLinks object exists, but neither property does
-        let url = "#"
-        return url;
-      }
-    }
-    else{ // imageLinks object doesn't exist
-      let url = "#";
-      return url;
-    }
-  };
+  // let url = () => {
+  //   if (imageLinks) { // Test if the imagesLinks object exists
+  //     if (imageLinks.thumbnail) { // Test if the thumbnail property exists
+  //       let url = imageLinks.thumbnail;
+  //       return url;
+  //     }
+  //     else if(imageLinks.smallThumbnail) { // Test if the smallThumbnail property exists
+  //       let url = imageLinks.smallThumbnail;
+  //       return url;
+  //     }
+  //     else { // imageLinks object exists, but neither property does
+  //       let url = "#"
+  //       return url;
+  //     }
+  //   }
+  //   else{ // imageLinks object doesn't exist
+  //     let url = "#";
+  //     return url;
+  //   }
+  // };
 
   return (
     <div>
       <h2>{title}</h2>
       <p>
-        <img src={url()} alt={title} />
+        <img src={thumbnail || smallThumbnail} alt={title} />
       </p>
-      <p>{authors}</p>
+      <p>by {authors ? authors.join(" , ") : "No Authors"}</p>
       <p>{description}</p>
-      <p>{amount()}</p>
+      <p>{listPrice && formatter.format(listPrice.amount)}</p>
+      <button onClick={() => addBook(title)}>Add</button>
     </div>
   );
+};
+
+Book.propTypes = {
+  book: PropTypes.shape({
+    id: PropTypes.string,
+    volumeInfo: PropTypes.shape({
+      imageLinks: PropTypes.shape({
+        thumbnail: PropTypes.string,
+        smallThumbnail: PropTypes.string,
+      }),
+      title: PropTypes.string.isRequired,
+      authors: PropTypes.array.isRequired,
+      description: PropTypes.string,
+    }),
+    saleInfo: PropTypes.shape({
+      listPrice: PropTypes.shape({
+        amount: PropTypes.number,
+      }),
+    }),
+  }),
 };
 
 export default Book;
