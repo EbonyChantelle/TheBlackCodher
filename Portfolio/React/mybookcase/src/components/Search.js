6d811b2e40622a52cd85cb0dbc1fb0f6e5a5087b
EbonyChantelle <ebonychantelle@icloud.com>
2020-10-13 21:17:03 +0100

    Session 7 Upadtes

---
+++ Portfolio/React/mybookcase/src/components/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Search (props) {
+  
+  function handleSubmit (event) {
+    event.preventDefault();
+    props.findBooks(props.keyword);
+  }
+
+  return (
+    <form className='search-form' onSubmit={handleSubmit}>
+      <input type="text" value={props.keyword} onChange={(event) => props.setKeyword(event.target.value)}/>
+      <button>Search</button>
+    </form>
+  );
+}
+
+export default Search;
