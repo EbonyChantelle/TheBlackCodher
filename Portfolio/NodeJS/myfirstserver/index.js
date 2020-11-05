394f3c7724162190c9ba12e8e32599ff65c83411
EbonyChantelle <ebonychantelle@icloud.com>
2020-11-05 22:34:38 +0000

    Creat CRUD API

--- Portfolio/NodeJS/myfirstserver/index.js
+++ Portfolio/NodeJS/myfirstserver/index.js
@@ -1,21 +1,33 @@
 const express = require('express')
 const cors = require('cors')
+const { ftruncateSync } = require('fs')
 
 const app = express()
 app.use(cors())
 
-const me = {
-  name: 'Ebony',
-  colour: 'Pink',
-  food: 'Salmon'
+const myProfile = {
+  name: 'Ebony'
 }
+
+const facts = [
+  'My favourite colour is pink',
+  'I like having salmon for dinner',
+  'Currently I am part of The Black Codher Bootcamp',
+  'I drive a MINI Paceman John Cooper Works'
+]
 
 app.get('/', (req, res) => {
   res.json({message: 'Code Is Frustrating'})
 })
 
 app.get('/about-me', (req, res) => {
-  res.json(me)
+  const randomIndex = Math.floor(Math.random
+    () * facts.length)
+    const randomFact = facts[randomIndex]
+  res.json({
+    ...myProfile,
+    fact: randomFact
+  })
 })
 
 app.listen(4000, () => {
