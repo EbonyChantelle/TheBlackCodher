3faa326d05b011b7a65e839edeb7542df3197438
EbonyChantelle <ebonychantelle@icloud.com>
2020-11-08 22:36:53 +0000

    Add Post Requests

--- Portfolio/NodeJS/mycrudapi/index.js
+++ Portfolio/NodeJS/mycrudapi/index.js
@@ -1,10 +1,15 @@
+// Getting Libraries
 const express = require('express')
 const cors = require('cors')
 
+// Using Libraries
 const app = express()
 app.use(cors())
+
+// Parsing application/JSON
 app.use(express.json())
 
+// Profile/Book Objects
 const profile = {
   forname: "Ebony",
   surname: "Gordon",
@@ -15,10 +20,11 @@ const profile = {
 const book = {
   title: "Queenie",
   author: "Candice Carty-Williams",
-  description: "A funny, clever, heartbreaking lightning bolt of a novel",
-  price: "£4.99"
+  published: 2017,
+  price: "£8.99"
 }
 
+// Database IDs
 const db = {
   profiles: {
     001: profile,
@@ -28,6 +34,7 @@ const db = {
   },
 }
 
+// Read Existing Data
 app.get('/profiles', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -42,6 +49,36 @@ app.get('/books', (req, res) => {
   })
 })
 
+//Add New Data (including fucntion to assign new IDs in corresponding order)
+app.post('/profiles', (req, res) => {
+
+  const existingIds = Object.keys(db.profiles)
+  const largestId = Math.max(...existingIds)
+  const newId = largestId + 1
+
+  db.profiles[newId] = req.body
+
+  res.status(201).json({
+    status: 'success',
+    message: `New profile created with id of ${newId}`
+  })
+})
+
+app.post('/books', (req, res) => {
+
+  const existingIds = Object.keys(db.books)
+  const largestId = Math.max(...existingIds)
+  const newId = largestId + 1
+
+  db.books[newId] = req.body
+
+  res.status(201).json({
+    status: 'success',
+    message: `New book created with id of ${newId}`
+  })
+})
+
+// Local Host URL
 app.listen(4000, () => {
   console.log('Server Running')
 })
