d907ac7ddfb13de98a4fdcdfb24e3244fad5090c
EbonyChantelle <ebonychantelle@icloud.com>
2020-11-09 12:44:22 +0000

    Add Patch/Put/Delete Requests

--- Portfolio/NodeJS/mycrudapi/index.js
+++ Portfolio/NodeJS/mycrudapi/index.js
@@ -34,7 +34,7 @@ const db = {
   },
 }
 
-// Read Existing Data
+// Read Existing Data/Read Matching Data (Otherwise Error)
 app.get('/profiles', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -47,6 +47,42 @@ app.get('/books', (req, res) => {
     status: 'success',
     data: db.books
   })
+})
+
+app.get('/profiles/:profileId', (req, res) => {
+  console.log(req.params.profileId)
+
+  const matchingProfile = db.profiles[req.params.profileId]
+
+  if (matchingProfile) {
+    res.json({
+      status: 'success',
+      data: matchingProfile
+    })
+    
+  } else {
+    res.status(404).json({
+      message: "Profile with that id not found"
+    })
+  }
+})
+
+app.get('/books/:bookId', (req, res) => {
+  console.log(req.params.bookId)
+
+  const matchingBook = db.books[req.params.bookId]
+
+  if (matchingBook) {
+    res.json({
+      status: 'success',
+      data: matchingBook
+    })
+    
+  } else {
+    res.status(404).json({
+      message: "Book with that id not found"
+    })
+  }
 })
 
 //Add New Data (including fucntion to assign new IDs in corresponding order)
@@ -75,6 +111,66 @@ app.post('/books', (req, res) => {
   res.status(201).json({
     status: 'success',
     message: `New book created with id of ${newId}`
+  })
+})
+
+// Amend Existing Data
+
+app.patch('/profiles/:profileId', (req, res) => {
+
+  db.profiles[req.params.profileId] = {
+    ...db.profiles[req.params.profileId],
+    ...req.body
+  }
+
+  res.status(200).json({
+    messages: "Profile Updated"
+  })
+})
+
+app.patch('/books/:bookId', (req, res) => {
+
+  db.books[req.params.bookId] = {
+    ...db.books[req.params.bookId],
+    ...req.body
+  }
+
+  res.status(200).json({
+    messages: "Book Updated"
+  })
+})
+
+// Overwrite Existing Data
+
+app.put('/profiles/:profileId', (req, res) => {
+  const overwriteProfile = req.params.profileId
+
+  db.profiles[overwriteProfile] = req.body
+
+  res.status(200).json({
+    message: "Profile Replaced"
+  })
+})
+
+app.put('/books/:bookId', (req, res) => {
+  const overwriteBook = req.params.bookId
+
+  db.books[overwriteBook] = req.body
+
+  res.status(200).json({
+    message: "Book Replaced"
+  })
+})
+
+// Delete Data
+
+app.delete('/profiles/:profileId', (req, res) => {
+
+  delete db.profiles[req.params.profileId]
+
+  res.status(200).json({
+    status: 'success',
+    message: "Profile Deleted"
   })
 })
 
